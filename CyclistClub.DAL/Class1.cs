56b0acff8df832573bc5616c4706a3d8f34da245
arolleaguekeng <arolleaguekeng@gmail.com>
2022-01-06 19:28:54 +0100

    Add project files.

---
+++ CyclistClub.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclistClub.DAL
+{
+    public class Class1
+    {
+    }
+}
